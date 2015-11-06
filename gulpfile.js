80855a128c741cfb54be2a8c2da7e046e22c86ca
dnomak <me@dnomak.com>
2015-11-07 00:03:01 +0300

    düzenlemeler yapıldı

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,39 @@
 
 'use strict';
 
-var gulp    = require('gulp'),
-    pkg     = require('./package.json'),
-    sass    = require('gulp-sass'),
-    csslint = require('gulp-csslint'),
-    rename  = require('gulp-rename');
+var gulp = require('gulp'),
+  pkg = require('./package.json'),
+  sass = require('gulp-sass'),
+  csslint = require('gulp-csslint'),
+  rename = require('gulp-rename');
 
 var tasks = {
-    sass:    function() {
-        return gulp.src('settings.scss')
-            .pipe(sass({
-                outputStyle: 'expanded'
-            }))
-            .pipe(rename(pkg.name + '.css'))
-            .pipe(gulp.dest('build'));
-    },
-    csslint: function() {
-        return gulp.src('build/' + pkg.name + '.css')
-            .pipe(csslint())
-            .pipe(csslint.reporter());
-    }
+  sass: function () {
+    return gulp.src('settings.scss')
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }))
+      .pipe(rename(pkg.name + '.css'))
+      .pipe(gulp.dest('build'));
+  },
+  csslint: function () {
+    return gulp.src('build/' + pkg.name + '.css')
+      .pipe(csslint())
+      .pipe(csslint.reporter());
+  }
 };
 
 gulp.task('sass', tasks.sass);
 
-gulp.task('styles', ['sass'], function() {
-    return tasks.csslint();
+gulp.task('styles', ['sass'], function () {
+  return tasks.csslint();
 });
 
-gulp.task('watch', ['styles'], function() {
-    // --------------------------
-    // watch:sass
-    // --------------------------
-    gulp.watch('**/*.scss', ['styles']);
+gulp.task('watch', ['styles'], function () {
+  // --------------------------
+  // watch:sass
+  // --------------------------
+  gulp.watch('**/*.scss', ['styles']);
 });
 
 gulp.task('default', ['watch']);
